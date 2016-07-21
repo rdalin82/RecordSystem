ba64d76bdc9225aca16da1b5a9b4c16448669358
rdalin82 <Robert.Dalin@gmail.com>
2016-07-20 22:29:36 -0500

    adding new record with parser and record

--- lib/record.rb
+++ lib/record.rb
@@ -1,4 +1,13 @@
 module FileSystem
   class Record
+    attr_reader :last_name, :first_name, :gender, :favorite_color, :date_of_birth
+    def initialize(input)
+      input_array = Parser.parse(input)
+      @last_name = input_array[0]
+      @first_name = input_array[1]
+      @gender = input_array[2]
+      @favorite_color = input_array[3]
+      @date_of_birth = input_array[4]      
+    end
   end
 end
--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,7 +1,29 @@
 require_relative './spec_helper'
 
 RSpec.describe FileSystem::Record do 
-  it "creates a blank record" do 
-    record = FileSystem::Record.new
+  let(:inputcomma) { "Dalin, Robert, Male, Blue, 12/05/1995" }
+  let(:inputpipe) { "Dalin | Robert | Male | Blue | 12/05/1995" }
+  it "creates a new record" do 
+    record = FileSystem::Record.new(inputcomma)
+  end
+  it "creates a new record and has a last name" do 
+    record = FileSystem::Record.new(inputcomma)
+    expect(record.last_name).to eq("Dalin")
+  end
+  it "creates a new record and has a first name" do 
+    record = FileSystem::Record.new(inputcomma)
+    expect(record.first_name).to eq("Robert")
+  end
+  it "creates a new record and has a gender" do 
+    record = FileSystem::Record.new(inputcomma)
+    expect(record.gender).to eq("Male")
+  end
+  it "creates a new record and has a favorite color" do 
+    record = FileSystem::Record.new(inputcomma)
+    expect(record.favorite_color).to eq("Blue")
+  end
+  it "creates a new record and has a birthday" do 
+    record = FileSystem::Record.new(inputcomma)
+    expect(record.date_of_birth).to eq("12/05/1995")
   end
 end
