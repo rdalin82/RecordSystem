d0add242dd3e863a6f553cf257cfd4f4c55ff3c9
rdalin82 <Robert.Dalin@gmail.com>
2016-07-20 21:39:52 -0500

    empty record class

---
+++ specs/record_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Record do 
+  it "creates a blank record" do 
+    record = Record.new
+  end
+end
---
+++ specs/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'rspec'
+require_relative '../lib/record'
+require_relative '../lib/parser'
+require_relative '../lib/file_system'
