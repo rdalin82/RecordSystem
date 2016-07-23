eb18e6d089fc2c3f64015732e3490ee1a662b346
rdalin82 <Robert.Dalin@gmail.com>
2016-07-23 16:27:12 -0500

    moved file_source method to module leve to use between reader and writer

--- lib/record_reader.rb
+++ lib/record_reader.rb
@@ -1,8 +1,5 @@
 module RecordSystem
   class RecordReader
-    def self.file_source
-      File.expand_path("../data/records.csv", File.dirname(__FILE__))
-    end
     def self.open(source=file_source)
       File.open(source, 'r')
     end
--- lib/record_system.rb
+++ lib/record_system.rb
@@ -1,2 +1,5 @@
 module RecordSystem
+  def file_source
+    File.expand_path("../data/records.csv", File.dirname(__FILE__))
+  end
 end
