3eb14a215db961a7555ff03a3c8dea19f78c4c2e
rdalin82 <Robert.Dalin@gmail.com>
2016-07-25 20:23:10 -0500

    updated RecordReader

--- lib/record_reader.rb
+++ lib/record_reader.rb
@@ -5,13 +5,15 @@ module RecordSystem
     end
     def self.load(**args)
       record_lines = [] 
+      parser = args[:parser] ||= Parser
+      validator = args[:validator] ||= Validator.new
       source = args[:source] ||= file_source
       record = args[:record] ||= Record
       file = File.open(source, 'r')
       while !file.eof? 
         line = file.readline
         if !line.chomp.empty?
-          record_lines << record.new(line)
+          record_lines << record.new(line, :parser=>parser, :validator=>validator)
         end
       end
       file.close
